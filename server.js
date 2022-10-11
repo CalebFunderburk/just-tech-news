9744777a97952fa5c5f19d01a8bf78da17368db4
Caleb <calebfunderburk@icloud.com>
2022-10-11 15:00:48 -0400

    added homepage functionality

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path')
+const exhbs = require('express-handlebars')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exhbs.create({})
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(routes);
 
+
+
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
